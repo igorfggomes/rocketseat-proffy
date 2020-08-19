12609d56c44db0ddc8ef319d580f04607979a399
igorfggomes <igor.fggomes@hotmail.com>
2020-08-19 15:49:26 -0300

    Testar métodos de requisição http

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,11 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-    const users = [
-        { name: 'Diego', age: 25 },
-        { name: 'Vini', age: 21 },
-    ];
-    
-    return response.json(users);
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello World' });
 });
 
-//localhost:3333/users
-
 app.listen(3333);
+
